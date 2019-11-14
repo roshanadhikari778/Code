b5b570f00813c3bc498ada9b5b9ce44a43fb9765
Roshan Adhikari <roshanadhikari778@gmail.com>
2019-11-14 17:34:52 +0000

    Update surveySolutions_API.R
    Updated libraries

--- surveySolutions_API.R
+++ surveySolutions_API.R
@@ -6,7 +6,6 @@
 # Notes: You will need to first create an R API user in Administrator. Instructions can be  
 # found on this link: https://rstudio-pubs-static.s3.amazonaws.com/239851_1bc298ae651c41c7a65e09ce82f9053f.html
 #---------------------------------------------------------------------------------------------
-library(networkD3)
 library(xml2)
 library(jsonlite)
 library(httr)
